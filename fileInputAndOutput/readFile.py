af5c3c5692560f6fb1c567f6223c307109d214dd
Bittu Raj <bitturaj124365@gmail.com>
2024-12-12 13:45:03 +0530

    Add files via upload

---
+++ fileInputAndOutput/readFile.py
@@ -0,0 +1,5 @@
+d=open("demo.txt","r")
+data=d.read()
+print(data)
+print(type(data))
+d.close()
---
+++ fileInputAndOutput/writeFile.py
@@ -0,0 +1,5 @@
+# w is used to write data
+
+f=open("demo.txt","w")
+f.write("Hii there, this is B2Raj.")
+f.close()
