fb6f0360692d4a4cd5ce98e223b3d6736e9dcaed
Bittu Raj <bitturaj124365@gmail.com>
2024-12-17 23:35:18 +0530

    Add files via upload

---
+++ Functions_/basic.py
@@ -0,0 +1,29 @@
+# def keyword is used to define function in python
+def printFunc():
+    print("Hii B2.")
+def calSum(a,b):
+    sum=a+b
+    print("Sum is:",sum)
+def calSub(a,b):
+    sub=a-b
+    print("Sub is:",sub)
+def calMul(a,b):
+    product=a*b
+    print("Product is:",product)
+def calDiv(a,b):
+    divide=a/b
+    print("Division is:",divide)
+def calRem(a,b):
+    remainder=a%b
+    print("Remainder is:",remainder)
+def calAverage(a,b,c):
+    average=(a+b+c)/3
+    print("Average is:",average)
+printFunc()
+printFunc()
+calSum(13,4)
+calSub(34,2)
+calMul(22,5)
+calDiv(10,3)
+calRem(10,3)
+calAverage(1,2,3)
